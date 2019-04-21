cf3d929144d6ade93faede3b99cc8b74912150f4
小野航 <onowataru@onowatarunoMacBook-puro-2.local>
2019-04-21 22:42:51 +0900

    helloを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def goodbye
-    render html: "goodbye"
+    render html: "goodbye-Hello"
   end
 
 end
